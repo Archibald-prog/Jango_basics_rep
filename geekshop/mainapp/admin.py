cd7072bc5cedd743b62471efbf141c49999f69a1
Archibald-prog <archpod@gmail.com>
2021-10-27 07:58:31 +0300

    ДЗ к Уроку 6  - добавил кнопки удаления товаров, поля для изменения количества товаров(AJAX) и декораторы

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -3,10 +3,9 @@ from mainapp.models import ProductCategory, Product
 from mainapp.models import Contact
 
 
-
 admin.site.register(ProductCategory)
 
 admin.site.register(Product)
 
 admin.site.register(Contact)
-# Register your models here.
+
